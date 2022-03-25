774df44a831424af33a12ec3ac2e2e8f8e21f077
eun-chongzzz <sksk4414@naver.com>
2022-03-25 15:26:33 +0900

    Spring 의존성

---
+++ SpringBasic/src/main/java/com/ict/controller/di/classfile/PopSinger.java
@@ -0,0 +1,11 @@
+package com.ict.controller.di.classfile;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PopSinger extends Singer{
+	
+	public void sing() {
+		System.out.println("팝가수가 영어로 노래를 합니다.");
+	}
+}
